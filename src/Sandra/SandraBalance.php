cd4ba17db4df7f28c03307d26f6709c6d57d20c0
shaban shaame <shaban@everdreamsoft.com>
2019-07-04 18:43:19 +0200

    initila setup

---
+++ src/Sandra/SandraBalance.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by EverdreamSoft.
+ * User: Shaban Shaame
+ * Date: 2019-07-04
+ * Time: 17:44
+ */
+
---
+++ tests/Sandra/TestSandraBalance.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by EverdreamSoft.
+ * User: Shaban Shaame
+ * Date: 2019-07-04
+ * Time: 17:46
+ */
+
+class TestSandraBalance extends \Ethereum\TestStatic
+{
+
+
+    public function testSandra(){
+
+
+    $this->assertEquals(1,1);
+
+
+    }
+
+
+
+
+
+}
